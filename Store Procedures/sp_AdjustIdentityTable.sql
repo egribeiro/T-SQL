3ca58c34e35fc7d8faa118d3f8e9ab6705420d3f
Edson Ribeiro <37663511+egribeiro@users.noreply.github.com>
2018-08-20 00:36:21 -0300

    Store Procedures/sp_AdjustIdentityTable.sql

---
+++ Store Procedures/sp_AdjustIdentityTable.sql
@@ -0,0 +1,75 @@
+IF OBJECT_ID(N'sp_AdjustIdentityTable',N'P') IS NOT NULL
+	DROP PROCEDURE [dbo].[sp_AdjustIdentityTable]
+GO
+-- ===========================================================================
+-- Author: Edson Ribeiro
+-- Create date: 19/08/2017
+-- Description: Sometimes after insert into table, the identity column jump 
+-- a range numbers and it to be ajusted with this procedure.
+-- ===========================================================================
+CREATE PROCEDURE [dbo].[sp_AdjustIdentityTable]
+(
+	@Table VARCHAR(100) 
+)
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+	
+-- ============================================================================================================================================================
+--	Variables of process
+-- ============================================================================================================================================================
+	BEGIN 
+	
+		DECLARE 
+			@Id1         VARCHAR(5)      = '',
+			@Id2         VARCHAR(5)      = '',
+			@SQL           NVARCHAR(MAX)   = '',
+			@Parameters    NVARCHAR(500)   = '',  
+			@Column        VARCHAR(50)     = ''
+	
+	END
+-- ============================================================================================================================================================
+--	Begin process
+-- ============================================================================================================================================================
+	BEGIN
+	
+		--Get identity column of table
+		SELECT 
+			@Column = c.name
+		FROM SYS.columns        AS C
+		INNER JOIN SYS.tables   AS T ON T.object_id = C.object_id
+		WHERE   
+			T.name = @Table
+		AND
+			C.is_identity = 1
+
+		IF ISNULL(@Column,'') != ''
+			BEGIN
+
+				--Get current identity of column 
+				SELECT @Id1 = CAST(IDENT_CURRENT (@Table) + 1 AS VARCHAR(10))
+				 
+				--Get last Id(Identity of the column)
+				SET @SQL = CONCAT('
+									SELECT 
+										@Id2OUT = CAST(MAX(',@Column,') + 1 AS VARCHAR(10)) 
+									FROM ',@Table,'
+									 
+								')
+				SET @Parameters = '@Id2OUT VARCHAR(5)'
+				
+				EXECUTE sp_ExecuteSQL   @SQL, 
+										@Parameters,
+										@Id2OUT = @UCod2 OUTPUT;  
+
+				--Checking if SGBD identity is bigger than last inserted in the table.
+				IF CAST(@Id1 AS INT) > CAST(@UCod2 AS INT)
+				BEGIN
+					SET @SQL = CONCAT(' DBCC CHECKIDENT (',@Table,', reseed, ',@UCod2,') ') 
+					EXEC ExecuteSQL @SQL
+				END
+			END
+	END
+		
+END
